c31d524e98dd330a2d256684325403a7848ae716
Lawrence-Tang <tangzongsheng@gmail.com>
2022-07-23 00:38:03 +0800

    fix script issue

--- distfeeds/install.sh
+++ distfeeds/install.sh
@@ -4,11 +4,11 @@ ROOTFS_DIR=$1
 
 (cd ${ROOTFS_DIR} && {
 	source usr/lib/os-release
-	cat > etc/opkg/distfeeds.conf <<EOL
+	cat >etc/opkg/distfeeds.conf <<EOF
 src/gz openwrt_base https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/base
 src/gz openwrt_luci https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/luci
 src/gz openwrt_packages https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/packages
 src/gz openwrt_routing https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/routing
 src/gz openwrt_telephony https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/telephony
-EOL    
+EOF
 })
