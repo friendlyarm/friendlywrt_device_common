2da7b1789efec402ceaef94b21a38ca200b57967
Lawrence-Tang <tangzongsheng@gmail.com>
2022-10-31 23:46:30 +0800

    change opkg source to cn-mirror, add r6s support

--- distfeeds/install.sh
+++ distfeeds/install.sh
@@ -5,10 +5,16 @@ ROOTFS_DIR=$1
 (cd ${ROOTFS_DIR} && {
 	source usr/lib/os-release
 	cat >etc/opkg/distfeeds.conf <<EOF
-src/gz openwrt_base https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/base
-src/gz openwrt_luci https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/luci
-src/gz openwrt_packages https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/packages
-src/gz openwrt_routing https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/routing
-src/gz openwrt_telephony https://downloads.openwrt.org/releases/${VERSION}/packages/aarch64_generic/telephony
+#
+# Use the following command to switch to the default source:
+#     sed -i -e 's/mirrors.cloud.tencent.com/downloads.openwrt.org/g' /etc/opkg/distfeeds.conf
+#     opkg update
+#
+
+src/gz openwrt_base https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/base
+src/gz openwrt_luci https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/luci
+src/gz openwrt_packages https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/packages
+src/gz openwrt_routing https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/routing
+src/gz openwrt_telephony https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/telephony
 EOF
 })
