eb813c6eb9d06ea538b4a8a201ac23a598b3c357
Lawrence-Tang <tangzongsheng@gmail.com>
2024-03-29 16:59:39 +0800

    update for openwrt-v23.05.3

--- distfeeds/install.sh
+++ distfeeds/install.sh
@@ -7,14 +7,14 @@ ROOTFS_DIR=$1
 	cat >etc/opkg/distfeeds.conf <<EOF
 #
 # Use the following command to switch to the default source:
-#     sed -i -e 's/mirrors.cloud.tencent.com/downloads.openwrt.org/g' /etc/opkg/distfeeds.conf
+#     sed -i -e 's/mirrors.ustc.edu.cn\/openwrt/downloads.openwrt.org/g' /etc/opkg/distfeeds.conf
 #     opkg update
 #
 
-src/gz openwrt_base https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/base
-src/gz openwrt_luci https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/luci
-src/gz openwrt_packages https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/packages
-src/gz openwrt_routing https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/routing
-src/gz openwrt_telephony https://mirrors.cloud.tencent.com/openwrt/releases/${VERSION}/packages/aarch64_generic/telephony
+src/gz openwrt_base https://mirrors.ustc.edu.cn/openwrt/releases/${VERSION}/packages/aarch64_generic/base
+src/gz openwrt_luci https://mirrors.ustc.edu.cn/openwrt/releases/${VERSION}/packages/aarch64_generic/luci
+src/gz openwrt_packages https://mirrors.ustc.edu.cn/openwrt/releases/${VERSION}/packages/aarch64_generic/packages
+src/gz openwrt_routing https://mirrors.ustc.edu.cn/openwrt/releases/${VERSION}/packages/aarch64_generic/routing
+src/gz openwrt_telephony https://mirrors.ustc.edu.cn/openwrt/releases/${VERSION}/packages/aarch64_generic/telephony
 EOF
 })
